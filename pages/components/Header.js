2a9f03de41bb8a5f9f866b76d0b0c41583d04b8b
Ignacio Rodrigues <ignacio.rodrigues@sdc.com.ar>
2021-05-17 17:53:29 -0300

    Fix: Update website

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -13,7 +13,7 @@ export default function Header() {
         <div className="py-6 flex items-center justify-between sm:mx-0px-4 px-4">
           <button type="button" className="group leading-6 font-medium flex items-center space-x-3 sm:space-x-4 hover:text-gray-600 transition-colors duration-200">
             <svg width="24" height="24" fill="none" className="text-gray-400 group-hover:text-gray-500 transition-colors duration-200">
-              <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+              <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path>
             </svg>
             <span>Buscar<span className="hidden sm:inline"> información</span></span>
             <span className="hidden sm:block text-gray-400 text-sm leading-5 py-0.5 px-1.5 border border-gray-300 rounded-md">
--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,4 @@ module.exports = {
     tailwindcss: {},
     autoprefixer: {},
   },
-  images: {
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-  },
 }
